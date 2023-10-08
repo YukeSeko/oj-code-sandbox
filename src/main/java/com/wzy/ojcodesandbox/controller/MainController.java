fe5a14895c84ff8819ebda8e6609a5e29ac694e0
王灼宇 <1286694258@qq.com>
2023-10-08 16:57:18 +0800

    first commit

---
+++ src/main/java/com/wzy/ojcodesandbox/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.wzy.ojcodesandbox.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 王灼宇
+ * @Since 2023/10/8 15:59
+ */
+@RestController
+public class MainController {
+
+
+}
